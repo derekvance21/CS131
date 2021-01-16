697e9c8544ae70063ad4fc0b6d72569dd06bff36
Derek Vance <derekvance21@gmail.com>
2021-01-15 19:11:22 -0800

    hw1.ml done, writing hw1test.ml next

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -21,7 +21,12 @@ let set_symdiff a b =
     )
   ) (set_union a b)
 
-let self_member s = s;;
+(* 
+  It is impossible to write a self_member s function because of OCaml's type system. 
+  A set s is of type 'a list, meaning members of s can only be of type 'a.
+  Thus, because s is of type 'a list but requires members to be of type 'a,
+  s cannot be a member of itself.
+ *)
 
 let rec computed_fixed_point eq f x = 
   let fx = f x in
@@ -32,3 +37,34 @@ let rec computed_fixed_point eq f x =
 type ('nonterminal, 'terminal) symbol =
   | N of 'nonterminal
   | T of 'terminal
+
+let get_reachable_symbols rules_and_reachable_symbols = 
+  let rec filter_non_terminals rhs = 
+    match rhs with
+    | [] -> []
+    | (N nts)::rst -> nts::(filter_non_terminals rst)
+    | hd::rst -> filter_non_terminals rst
+  in
+
+  let rec get_reachable_symbols_rec rules reachable_symbols = 
+  match rules with 
+  | [] -> (rules, reachable_symbols)
+  | (lhs, rhs)::rest_rules when subset [lhs] reachable_symbols ->
+    get_reachable_symbols_rec
+      rest_rules (set_union reachable_symbols (filter_non_terminals rhs))
+  | _ -> get_reachable_symbols_rec (List.tl rules) reachable_symbols
+  in
+
+  let (rules, reachable_symbols) = rules_and_reachable_symbols in
+  let (_, calc_reachable_symbols) = get_reachable_symbols_rec rules reachable_symbols in
+  (rules, calc_reachable_symbols)
+
+let filter_reachable g = 
+  let equal_reachable_symbol_sets a b = 
+  let (_, a2) = a and (_, b2) = b in
+    equal_sets a2 b2 
+  in
+
+  let (start_symbol, rules) = g in
+  let (_, reachable_symbols) = computed_fixed_point equal_reachable_symbol_sets get_reachable_symbols (rules, [start_symbol]) in
+  (start_symbol, List.filter (fun rule -> let (lhs, _) = rule in subset [lhs] reachable_symbols) rules)
